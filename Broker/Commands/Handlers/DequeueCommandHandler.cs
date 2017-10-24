a5dcfbd92195877e50a9f2883f40d28e47d9e937
Nicu Maxian <maxiannicu@gmail.com>
2017-10-24 12:26:11 +0300

    Added dequeue command

---
+++ Broker/Commands/Handlers/DequeueCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Broker.Commands.Attributes;
+using Broker.Queues.Services;
+using Broker.Server;
+using Utils.Packets;
+
+namespace Broker.Commands.Handlers
+{
+    [Command("dequeue")]
+    public class DequeueCommandHandler : AbstractRegexHandler
+    {
+        private readonly IQueueService _queueService;
+
+        public DequeueCommandHandler(IQueueService queueService) : base(@"^([\w\d\.\?\*]*){1}$")
+        {
+            _queueService = queueService;
+        }
+
+        public override Packet GetData(string[] data, ClientContext context)
+        {
+            var nextMessage = _queueService.GetNextMessage(data[0]);
+
+            if (nextMessage == null)
+            {
+                return Packet.Ok("No items to dequeue");
+            }
+            return Packet.Message(nextMessage.QueueIdentifier, nextMessage.Content);
+        }
+    }
+}
